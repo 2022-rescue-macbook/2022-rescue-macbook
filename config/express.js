dff170e386657133bf4712f59304a54e58edd8f1
Serin-Yoon <bcode429@gmail.com>
2022-09-23 21:52:30 +0900

    delete unused modules

--- config/express.js
+++ config/express.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const compression = require('compression');
-const methodOverride = require('method-override');
-var cors = require('cors');
 
 module.exports = function () {
     const app = express();
@@ -9,8 +7,6 @@ module.exports = function () {
     app.use(compression());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
-    app.use(methodOverride());
-    app.use(cors());
 
     require('../src/routes/indexRoute')(app);
 
